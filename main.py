3bd3abe7927c6fb88d5c284a46e520b962530100
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2023-09-15 22:46:09 +0100

    basic neatening

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#! /usr/bin/env python3
+
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import seaborn as sns
 
@@ -7,22 +9,17 @@ sns.set(rc={"figure.figsize": (14, 9)})
 sns.set_style("darkgrid")
 
 def sigmoid(t):
-
-	t_trans = 10.0
-
-	k = 10.0/t_trans
-
-	return np.reciprocal(1 + np.exp(-k*t))
+  t_trans = 10.0
+  k = 10.0/t_trans
+  return np.reciprocal(1 + np.exp(-k*t))
 
 def sigmoid_grad(t, t_on):
-	
-    fx = sigmoid(t - t_on)
-    
-    return fx*(1 - fx)
+  fx = sigmoid(t - t_on)
+  return fx*(1 - fx)
 
 def SLS(t, g0, g1, tau):
-	return g0 + g1*np.exp(-t/tau)
-	
+  return g0 + g1*np.exp(-t/tau)
+
 ############
 # data get #
 ############
@@ -49,13 +46,13 @@ convolved = np.multiply(np.convolve(sls_t, de_dt, mode='full')[0:len(t)], np.gra
 
 # global set up
 grepeat = True
-	
-fig, axarr = pl.subplots(2, 2)
+
+fig, axarr = plt.subplots(2, 2)
 
 # axarr[0,0] set up
 e_line, = axarr[0,0].plot(t, e, "-", label="strain load", color="#566573")
 de_line00, = axarr[0,0].plot(t, de_dt, "--", label="d(strain)/dt", color="#3385ff")
-dotfollow_line, = axarr[0,0].plot(t[0], e[0], "o", color="#dc7633") 
+dotfollow_line, = axarr[0,0].plot(t[0], e[0], "o", color="#dc7633")
 
 axarr[0,0].legend(loc='lower left')
 
@@ -65,20 +62,20 @@ axarr[0,0].set_ylim(-0.4, 1.2)
 lines00 = (e_line, de_line00, dotfollow_line)
 
 def init00():
-	
-	lines00[0].set_data(t, e)
-	lines00[1].set_data(t, de_dt)
-		
-	return lines00
-	
+
+  lines00[0].set_data(t, e)
+  lines00[1].set_data(t, de_dt)
+
+  return lines00
+
 def animate00(i):
-	
-	lines00[2].set_data(t[i], e[i]) 
-	
-	return lines00
+
+  lines00[2].set_data(t[i], e[i])
+
+  return lines00
 
 ani00 = anim.FuncAnimation(fig, animate00, np.arange(1, length-1), interval=1, init_func=init00, blit=True, repeat=grepeat)
-	
+
 # axarr[1,0] set up
 axarr[1,0].plot(t, sls_t, label="G_SLS(t)", color="#00b300")
 axarr[1,0].legend(loc='lower left')
@@ -96,16 +93,12 @@ sls_line, = axarr[0,1].plot(t, sls_t_padded[(2*length - 1):(3*length - 1)], '-.'
 lines01 = (de_line, sls_line)
 
 def init01():
-	
-	lines01[0].set_data(t, de_dt)
-	    
-	return lines01
+  lines01[0].set_data(t, de_dt)
+  return lines01
 
 def animate01(i):
-	
-	lines01[1].set_ydata(sls_t_padded[(2*length - i):(3*length - i)])
-	
-	return lines01
+  lines01[1].set_ydata(sls_t_padded[(2*length - i):(3*length - i)])
+  return lines01
 
 axarr[0,1].legend(loc='lower left')
 ani01 = anim.FuncAnimation(fig, animate01, np.arange(1, length-1), interval=1, init_func=init01, blit=True, repeat=grepeat)
@@ -122,20 +115,15 @@ multiplied_line, = axarr[1,1].plot(t[0], multiplied_scaling*np.multiply(de_dt, s
 lines11 = (convolved_line, multiplied_line)
 
 def init11():
-	
-	return lines11
-	
+  return lines11
+
 def animate11(i):
-	
-	lines11[0].set_data(t[0:i], convolved[0:i])
-	#~ lines11[1].set_data(t[0:i], multiplied_scaling*np.multiply(de_dt, sls_t_padded[(2*length - i):(3*length - i)])[0:i])
-	lines11[1].set_data(t, multiplied_scaling*np.multiply(de_dt, sls_t_padded[(2*length - i):(3*length - i)]))
-	
-	return lines11
+  lines11[0].set_data(t[0:i], convolved[0:i])
+  #~ lines11[1].set_data(t[0:i], multiplied_scaling*np.multiply(de_dt, sls_t_padded[(2*length - i):(3*length - i)])[0:i])
+  lines11[1].set_data(t, multiplied_scaling*np.multiply(de_dt, sls_t_padded[(2*length - i):(3*length - i)]))
+  return lines11
 
 axarr[1,1].legend(loc='lower left')
 ani11 = anim.FuncAnimation(fig, animate11, np.arange(1, length-1), interval=1, init_func=init11, blit=True, repeat=grepeat)
 
-pl.show()
-
-
+plt.show()
